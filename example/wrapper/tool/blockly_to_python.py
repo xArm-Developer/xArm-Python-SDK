63d09cd55d8bbef9be27fbc331da275783c7a8e2
vinman <vinman.wen@ufactory.cc>
2019-03-21 18:14:54 +0800

    update

--- example/wrapper/tool/blockly_to_python.py
+++ example/wrapper/tool/blockly_to_python.py
@@ -15,7 +15,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
 from xarm.tools.xml_tool import XmlTool
 
 # blockly app path
-source_path = 'C:\\Users\\ufactory\\.UFACTORY\projects\\test\\xarm7\\app\\myapp\\233\\app.xml'
+source_path = '../example.xml'
 # the path is the python code to save
 target_path = './py_blockly.py'
 xml = XmlTool(source_path)

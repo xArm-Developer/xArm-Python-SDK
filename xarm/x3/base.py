972aeacf7d6177c912f84749b265ad1707269f08
vinman <vinman.wen@ufactory.cc>
2020-09-04 10:36:47 +0800

    fix error=1

--- xarm/x3/base.py
+++ xarm/x3/base.py
@@ -1076,7 +1076,7 @@ class Base(Events):
                 return
             self._gravity_direction = convert.bytes_to_fp32s(rx_data[133:3*4 + 133], 3)
 
-            if error_code in [10, 11, 12, 13, 14, 15, 16, 17, 19, 28]:
+            if error_code in [1, 10, 11, 12, 13, 14, 15, 16, 17, 19, 28]:
                 self.modbus_baud = -1
                 self.robotiq_is_activated = False
                 self.gripper_is_enabled = False

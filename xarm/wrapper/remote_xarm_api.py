dba4bda7319c4389fb9a8e666b6c1fee3c41513e
vinman <vinman.wen@ufactory.cc>
2020-11-12 10:46:41 +0800

    update

--- xarm/wrapper/remote_xarm_api.py
+++ xarm/wrapper/remote_xarm_api.py
@@ -11,51 +11,50 @@ import functools
 from .xarm_api import XArmAPI
 
 
-def _call_studio_api(*args, **kwargs):
-    api_name = kwargs.pop('api_name', None)
-    ip = kwargs.pop('ip', None)
-    path = kwargs.pop('path', 'api')
-    if ip and api_name:
-        r = requests.post('http://{}:18333/{}'.format(ip, path), data=json.dumps({
-            'cmd': api_name, 'args': args, 'kwargs': kwargs
-        }), timeout=(5, None))
-        if r.status_code == 200:
-            res = r.json()
-            if 'info' in res:
-                return -1, res['info']
-            return res['res']
-        else:
-            print('request failed, http_status_code={}'.format(r.status_code))
-    else:
-        print('ip or api_name is empty, ip={}, api_name={}'.format(ip, api_name))
-
-
-class _RemoteXArm(object):
-    def __init__(self, ip, _arm):
-        self.ip = ip
-        self._arm = _arm
-
-    def __getattr__(self, item, *args, **kwargs):
-        kwargs['ip'] = self.ip
-        kwargs['api_name'] = item
-        attr = getattr(self._arm, item)
-        remote_api = functools.partial(_call_studio_api, *args, **kwargs)
-        return remote_api if callable(attr) else remote_api()
-
-
 class RemoteXArmAPI(XArmAPI):
+    class __RemoteXArm:
+        def __init__(self, _ip, _path, _arm):
+            self._ip = _ip
+            self._path = _path
+            self._arm = _arm
+
+        def __getattr__(self, item):
+            attr = getattr(self._arm, item)
+            remote_api = functools.partial(self.__call_studio_api, api_name=item)
+            return remote_api if callable(attr) else remote_api()
+
+        def __call_studio_api(self, *args, **kwargs):
+            api_name = kwargs.pop('api_name', None)
+            ip = kwargs.pop('ip', self._ip)
+            path = kwargs.pop('path', self._path)
+            if ip and api_name:
+                r = requests.post('http://{}:18333/{}'.format(ip, path), data=json.dumps({
+                    'cmd': api_name, 'args': args, 'kwargs': kwargs
+                }), timeout=(5, None))
+                if r.status_code == 200:
+                    res = r.json()
+                    if 'info' in res:
+                        print(res['info'])
+                    else:
+                        return res['res']
+                else:
+                    print('request failed, http_status_code={}'.format(r.status_code))
+            else:
+                print('ip or api_name is empty, ip={}, api_name={}'.format(ip, api_name))
+
+        def delete_blockly_app(self, name):
+            return self.__call_studio_api(None, 0, {
+                'parentPath': name,
+                'selectNode': {
+                    'type': 'file'
+                }
+            }, api_name='app_delete_item', path='cmd')
+
     def __init__(self, ip):
-        import warnings
-        warnings.warn("don't use it for now, just for debugging")
+        # import warnings
+        # warnings.warn("don't use it for now, just for debugging")
         XArmAPI.__init__(self, do_not_open=True)
-        self.__ip = ip
-        self.__old_arm = self._arm
-        self._arm = _RemoteXArm(ip, self.__old_arm)
+        self._arm = self.__RemoteXArm(ip, 'api', self._arm)
 
     def delete_blockly_app(self, name):
-        return _call_studio_api(None, 0, {
-            'parentPath': name,
-            'selectNode': {
-                'type': 'file'
-            }
-        }, api_name='app_delete_item', path='cmd', ip=self.__ip)
+        return self._arm.delete_blockly_app(name)

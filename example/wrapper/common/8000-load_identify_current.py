ced860bd258e54d4c8dd82d2ff378cf29b5f0475
Vinman <vinman.cub@gmail.com>
2021-07-28 14:50:58 +0800

    add iden load example

--- example/wrapper/common/8000-load_identify_current.py
+++ example/wrapper/common/8000-load_identify_current.py
@@ -51,37 +51,8 @@ def progress(item):
 
 arm.register_iden_progress_changed_callback(progress)
 start_time = time.time()
-arm.ft_sensor_enable(1)
-code, result = arm.ft_sensor_iden_load()
+code, result = arm.iden_tcp_load()
 end_time = time.time()
 print('code={}, result={}, cost_time={}'.format(code, result, end_time - start_time))
 arm.release_iden_progress_changed_callback(progress)
-arm.ft_sensor_app_set(0)
-arm.ft_sensor_enable(0)
 arm.disconnect()
-
-# arm.ft_sensor_enable(1)
-# # load indenrify ?
-# response = input("\nDo you need Load Calibration?  y(es) / n(o):\n")
-# if response == 'y':
-#     print("Identification process start, please wait for finish..")
-#     result = arm.ft_sensor_iden_load()[1]
-#
-#     print("\nIdentification result: ")
-#     print(result)
-#     response = input("\nContinue?  y(es) / n(o):\n")
-#     if response == 'n':
-#         print("Bye~")
-#         exit()
-#     arm.ft_sensor_cali_load(result)
-# else:
-#     # no need to identify and write load parameters：
-#     # [load mass : (kg)，Centroid x : (m)，Centroid y : (m)，Centroid z :(m)，offset_Fx (N)，offset_Fx (N)，offset_Fx(N)，offset_Mx(Nm)，offset_My (Nm)，offset_Mz (Nm)]
-#     result = [0.7131531834602356, -0.0005494913784787059, -0.0026768327224999666, 0.06637067347764969, -17.749963760375977, 2.7701117992401123, -30.62084197998047, 0.13900014758110046, -0.37988412380218506, -0.14760535955429077]
-#     arm.ft_sensor_cali_load(result)
-#
-# # wait vaild
-# time.sleep(1)
-# arm.ft_sensor_app_set(0)
-# arm.ft_sensor_enable(0)
-# arm.disconnect()

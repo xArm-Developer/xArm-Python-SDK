78c431ab857f1a1818519668c56d0ef5e16d9da6
rock <chenziwu@ufactory.cc>
2021-09-09 12:29:27 +0800

    add linear track position range describe

--- xarm/wrapper/xarm_api.py
+++ xarm/wrapper/xarm_api.py
@@ -3196,6 +3196,9 @@ class XArmAPI(object):
             1. only available if firmware_version >= 1.8.0
         
         :param pos: position. Integer between 0 and 700/1000/1500mm.
+            If SN start with AL1300 the position range is 0~700mm.
+            If SN start with AL1301 the position range is 0~1000mm.
+            If SN start with AL1302 the position range is 0~1500mm.
         :param speed: speed of the linear track. Integer between 1 and 1000mm/s. default is not set
         :param wait: wait to motion finish or not, default is True
         :param timeout: wait timeout, seconds, default is 100s.

f8a531584bd444d509c81d5c607806198ae735c5
Vinman <vinman.cub@gmail.com>
2022-10-31 10:24:26 +0800

    [Fix] fix the coroutine attribute had removed from asyncio (Python3.11)

--- xarm/core/comm/__init__.py
+++ xarm/core/comm/__init__.py
@@ -1,7 +1,5 @@
 try:
     from .serial_port import SerialPort
 except:
-    import warnings
-    warnings.warn('serial module is not found, if you want to connect to xArm with serial, please `pip install pyserial==3.4`')
-    SerialPort = object
+    SerialPort = None
 from .socket_port import SocketPort

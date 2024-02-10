27d8482a28b455eabf24ebaca95103abd9483f65
adityaj12 <adityajagarlamudi@gmail.com>
2024-02-09 19:51:51 -0600

    Updated num_reader, kinda works

--- computer_vision/number_detection/num_reader.py
+++ computer_vision/number_detection/num_reader.py
@@ -20,14 +20,29 @@ import cv2
 import pytesseract
 from pytesseract import image_to_string
 
-img = cv2.imread("IMG_5917.jpg")
-gry = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-thr = cv2.threshold(gry, 60, 225,
-                    cv2.THRESH_BINARY_INV)[1]
+img = cv2.imread("C:\\Users\\adity\\OneDrive\\Documents\\PCR\\PCR_Automation\\computer_vision\\number_detection\\IMG_5916.jpg")
 
+# crop_img = img[:,:]
+# cv2.imshow("cropped", crop_img)
+# cv2.waitKey(0)
+
+gry = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cv2.imshow("test", gry)
+cv2.waitKey(0)
+
+thr = cv2.threshold(gry, 45, 225,
+                    cv2.THRESH_BINARY_INV)[1]
 cv2.imshow("test", thr)
 cv2.waitKey(0)
 
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+
 txt = image_to_string(thr, config="--psm 7")
-print("".join([t for t in txt if t != '|']).strip())
+digits = "".join([t for t in txt if t != '|']).strip()
+
+chars = []
+for digit in digits:
+    if digit.isnumeric():
+        chars.append(digit)
+        
+print("Result: " + chars[0] + chars[1] + "." + chars[2])

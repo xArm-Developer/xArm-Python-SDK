eea8d7915aebaa94b646d8a655f121fdbdd5e057
adityaj12 <adityajagarlamudi@gmail.com>
2024-02-23 19:36:15 -0600

    Updated num_reader.py

--- computer_vision/number_detection/num_reader.py
+++ computer_vision/number_detection/num_reader.py
@@ -15,34 +15,37 @@
     
 #     cv2.waitKey(0)
     
-
+#imports
 import cv2
 import pytesseract
 from pytesseract import image_to_string
 
-img = cv2.imread("C:\\Users\\adity\\OneDrive\\Documents\\PCR\\PCR_Automation\\computer_vision\\number_detection\\IMG_5916.jpg")
+#importing image to extract numbers from
+img = cv2.imread("C:\\Users\\adity\\OneDrive\\Documents\\PCR\\PCR_Automation\\computer_vision\\number_detection\\14-5.jpg")
 
-# crop_img = img[:,:]
-# cv2.imshow("cropped", crop_img)
-# cv2.waitKey(0)
-
+#converting to grayscale
 gry = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 cv2.imshow("test", gry)
 cv2.waitKey(0)
 
+#setting threshold to make all pixels white or black
 thr = cv2.threshold(gry, 45, 225,
                     cv2.THRESH_BINARY_INV)[1]
 cv2.imshow("test", thr)
 cv2.waitKey(0)
 
+#to use pytesseract
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
-txt = image_to_string(thr, config="--psm 7")
+#extracting text from the image as a single line
+txt = image_to_string(thr, config='--psm 10  --oem 3 -c tessedit_char_whitelist=0123456789')
 digits = "".join([t for t in txt if t != '|']).strip()
 
+#only keeping numeric digits
 chars = []
 for digit in digits:
     if digit.isnumeric():
         chars.append(digit)
         
+print(chars)
 print("Result: " + chars[0] + chars[1] + "." + chars[2])

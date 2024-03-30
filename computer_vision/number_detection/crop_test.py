191b0163f3cd64ba9deb0ff4e119c4f900480e5f
adityaj12 <adityajagarlamudi@gmail.com>
2024-03-29 20:01:32 -0500

    messed with webcam and tested height

--- computer_vision/number_detection/crop_test.py
+++ computer_vision/number_detection/crop_test.py
@@ -3,7 +3,6 @@ import pytesseract
 from pytesseract import image_to_string
 import os
 import numpy as np
-import time
 
 def num_reader(img):    
     
@@ -13,8 +12,8 @@ def num_reader(img):
     
     # converting to grayscale
     gry = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # cv2.imshow("test", gry)
-    # cv2.waitKey(0)
+    cv2.imshow("test", gry)
+    cv2.waitKey(0)
     
     # # blur
     # blur = cv2.GaussianBlur(gry, (0,0), sigmaX=33, sigmaY=33)
@@ -44,8 +43,8 @@ def num_reader(img):
         # print("In",filename,"detecting",chars)
         if len(chars) == 3:
             print("Result: " + chars[0] + chars[1] + "." + chars[2])
-            # cv2.imshow("test", thr)
-            # cv2.waitKey(0)
+            cv2.imshow("test", thr)
+            cv2.waitKey(0)
             
             # cv2.imshow("test", morph)
             # cv2.waitKey(0)
@@ -53,49 +52,7 @@ def num_reader(img):
         else:
             continue
 
-cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
+img = cv2.imread("computer_vision/number_detection/crop_test.png")
+img = img[335:400, 450:650]
 
-# set lowest point of webcam 5 inches above the surface
-while True:
-    ret, frame = cap.read()
-    
-    if not ret:
-        break
-    
-    # crop frame first
-    num_reader(frame)
-    cv2.imshow("test", frame)
-
-# print(files_list)
-
-#adding a comment to show git stuff
-
-#importing image to extract numbers from
-
-# img = cv2.imread("/Users/snaehashriram/Coding/PCR_Automation/computer_vision/number_detection/14-5.jpg")
-# #converting to grayscale
-# gry = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-# cv2.imshow("test", gry)
-# cv2.waitKey(0)
-
-# #setting threshold to make all pixels white or black
-# thr = cv2.threshold(gry, 45, 225,
-#                     cv2.THRESH_BINARY_INV)[1]
-# cv2.imshow("test", thr)
-# cv2.waitKey(0)
-
-# #to use pytesseract
-# pytesseract.pytesseract.tesseract_cmd = '/opt/homebrew/bin/tesseract'
-
-# #extracting text from the image as a single line
-# txt = image_to_string(thr, config='--psm 10  --oem 3 -c tessedit_char_whitelist=0123456789')
-# digits = "".join([t for t in txt if t != '|']).strip()
-
-# #only keeping numeric digits
-# chars = []
-# for digit in digits:
-#     if digit.isnumeric():
-#         chars.append(digit)
-        
-# print(chars)
-# print("Result: " + chars[0] + chars[1] + "." + chars[2])
+num_reader(img)

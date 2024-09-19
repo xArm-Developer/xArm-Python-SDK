b583ca99382030a07de3b980ce3cb4ab6d6ab791
Vishal Kantharaju <vishal.kantharaju@gmail.com>
2024-09-19 14:12:46 -0500

    reorganized number detection code, sorted testing images

--- computer_vision/number_detection/number_detection.py
+++ computer_vision/number_detection/number_detection.py
@@ -4,12 +4,13 @@ from pytesseract import image_to_string
 import os
 import numpy as np
 import time
+import platform
 
 def num_reader(img):    
     
     # to use pytesseract
     # pytesseract.pytesseract.tesseract_cmd = '/opt/homebrew/bin/tesseract'
-    pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+    # pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
     
     # converting to grayscale
     gry = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -34,6 +35,7 @@ def num_reader(img):
         # extracting text from the image as a single line
         txt = image_to_string(thr, config='--psm 10  --oem 3 -c tessedit_char_whitelist=0123456789')
         digits = "".join([t for t in txt if t != '|']).strip()
+        print(f"Extracted digits: {digits}")
 
         # only keeping numeric digits
         chars = []
@@ -53,49 +55,55 @@ def num_reader(img):
         else:
             continue
 
-cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
+def read_camera(): 
+    current_os = platform.system()
 
-# set lowest point of webcam 5 inches above the surface
-while True:
-    ret, frame = cap.read()
-    
-    if not ret:
-        break
-    
-    # crop frame first
-    num_reader(frame)
-    cv2.imshow("test", frame)
+    if current_os == "Windows":
+        cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)  
+    elif current_os == "Darwin": 
+        cap = cv2.VideoCapture(0, cv2.CAP_AVFOUNDATION)
+    elif current_os == "Linux":
+        cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
+    else:
+        cap = cv2.VideoCapture(0)
 
-# print(files_list)
-
-#adding a comment to show git stuff
-
-#importing image to extract numbers from
-
-# img = cv2.imread("/Users/snaehashriram/Coding/PCR_Automation/computer_vision/number_detection/14-5.jpg")
-# #converting to grayscale
-# gry = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-# cv2.imshow("test", gry)
-# cv2.waitKey(0)
-
-# #setting threshold to make all pixels white or black
-# thr = cv2.threshold(gry, 45, 225,
-#                     cv2.THRESH_BINARY_INV)[1]
-# cv2.imshow("test", thr)
-# cv2.waitKey(0)
-
-# #to use pytesseract
-# pytesseract.pytesseract.tesseract_cmd = '/opt/homebrew/bin/tesseract'
-
-# #extracting text from the image as a single line
-# txt = image_to_string(thr, config='--psm 10  --oem 3 -c tessedit_char_whitelist=0123456789')
-# digits = "".join([t for t in txt if t != '|']).strip()
-
-# #only keeping numeric digits
-# chars = []
-# for digit in digits:
-#     if digit.isnumeric():
-#         chars.append(digit)
+    # set lowest point of webcam 5 inches above the surface
+    while True:
+        ret, frame = cap.read()
         
-# print(chars)
-# print("Result: " + chars[0] + chars[1] + "." + chars[2])
+        if not ret:
+            break
+        
+        # crop frame first
+        num_reader(frame)
+        cv2.imshow("test", frame)
+        cv2.waitKey(1)
+
+def read_img(img_path: str, show_images= False):
+    img = cv2.imread("test/IMG_2554(1).JPG")
+    
+    #converting to grayscale
+    gry = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    if show_images:
+        cv2.imshow("test", gry)
+        cv2.waitKey(0)
+
+    #setting threshold to make all pixels white or black
+    thr = cv2.threshold(gry, 45, 225,
+                        cv2.THRESH_BINARY_INV)[1]
+    if show_images:
+        cv2.imshow("test", thr)
+        cv2.waitKey(0)
+
+    #extracting text from the image as a single line
+    txt = image_to_string(thr, config='--psm 10  --oem 3 -c tessedit_char_whitelist=0123456789')
+    digits = "".join([t for t in txt if t != '|']).strip()
+
+    #only keeping numeric digits
+    chars = []
+    for digit in digits:
+        print(digits)
+        if digit.isnumeric():
+            chars.append(digit)
+            
+    print(chars)
